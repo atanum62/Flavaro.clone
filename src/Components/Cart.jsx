43d7970e6ec341561e81f878036a23a17d89965d
atanum62 <atanumaity62899@gmial.com>
2025-01-16 13:02:34 +0530

    second commit

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -25,18 +25,24 @@ const Cart = () => {
             className="border-2 border-gray-600 text-gray-600 font-bold text-xl hover:text-red-300 hover:border-red-300 rounded-md "
           />
         </div>
-        {cartItems.map((food) => {
-          return (
-            <Itemcart
-              key={food.id}
-              id={food.id}
-              name={food.name}
-              price={food.price}
-              img={food.img}
-              qty={food.qty}
-            />
-          );
-        })}
+        {cartItems.length > 0 ? (
+          cartItems.map((food) => {
+            return (
+              <Itemcart
+                key={food.id}
+                id={food.id}
+                name={food.name}
+                price={food.price}
+                img={food.img}
+                qty={food.qty}
+              />
+            );
+          })
+        ) : (
+          <h2 className="text-center text-xl font-bold text-gray-800">
+            Your cart is empty
+          </h2>
+        )}
 
         <div className="absolute bottom-0">
           <h3 className="font-semibold text-gray-800">Items:1</h3>
